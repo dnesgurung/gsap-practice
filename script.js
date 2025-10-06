19503fba2afe9a0511126ea9884bfb9b9a4bbb08
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-10-06 20:56:00 +0100

    further exercises for timeline and text

--- script.js
+++ script.js
@@ -11,25 +11,67 @@ gsap.to('#box1', {
 })
 */
 
-gsap.to('#box2', {
-    x: 1000,
-    duration: 2,
-    delay: 1,
-    rotate: 360,
-    backgroundColor: 'white',
-    borderRadius: "50%",
-    scale: 0.5,
-    repeat: -1,
-    yoyo: true
+// gsap.to('#box2', {
+//     x: 1000,
+//     duration: 2,
+//     delay: 1,
+//     rotate: 360,
+//     backgroundColor: 'white',
+//     borderRadius: "50%",
+//     scale: 0.5,
+//     repeat: -1,
+//     yoyo: true
 
-})
+// })
     
 
-gsap.from("h1", {
-  opacity: 0,
-  duration: 1,
-  delay: 1,
-  y: 30,
-  stagger: 0.3,
-  repeat: -1,
-});
+// gsap.from("h1", {
+//   opacity: 0,
+//   duration: 1,
+//   delay: 1,
+//   y: 30,
+//   stagger: 0.3,
+//   repeat: -1,
+// });
+
+// ***** Timeline *****
+
+// const tl = gsap.timeline();
+
+// tl.to("#box1", {
+//     x:1200,
+//     rotate: 360,
+//     delay: 1
+// })
+
+// tl.to("#box2", {
+//     x: 900,
+//     duration: 1.5
+// })
+
+// tl.to('#box3', {
+//     x: 400,
+//     rotate: 180,
+// })
+
+const tl = gsap.timeline();
+
+tl.from("h2", {
+    y: -20,
+    opacity: 0,
+    duration: 1,
+    delay: 0.5
+})
+tl.from("h4", {
+    y: -20,
+    opacity: 0,
+    duration: 1,
+   stagger: 0.3
+})
+tl.from("h1", {
+    y:20,
+    opacity:0,
+    duration: 0.5,
+    scale: 0.2,
+   
+})

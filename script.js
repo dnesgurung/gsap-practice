7e13d278ac7bfe49ba00480b78ae389f6b2d9347
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-10-06 01:12:53 +0100

    text animation basics

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-gsap.to('#box', {
+/*
+gsap.to('#box1', {
     x: 1000,
     duration: 2,
-    delay: 1
+    delay: 1,
+    rotate: 360,
+    scale: 0.5,
+    backgroundColor: 'white',
+    borderRadius: "50%"
 
 })
+
+gsap.from('#box2', {
+    x: 1000,
+    duration: 2,
+    delay: 1,
+    rotate: 360,
+    scale: 0.5,
+    backgroundColor: 'white',
+    borderRadius: "50%"
+
+})
+    */
+
+gsap.from("h1", {
+  opacity: 0,
+  duration: 1,
+  delay: 1,
+  y: 30,
+  stagger: 0.3,
+});

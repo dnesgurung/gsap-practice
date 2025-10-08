3d3d7d1a8a065ab6bbb410170f8791406cd85457
dnesgurung <174604785+dnesgurung@users.noreply.github.com>
2025-10-08 18:33:17 +0100

    implement attribute changes to string

--- SVG animations/script.js
+++ SVG animations/script.js
@@ -1,1 +1,28 @@
-const initialPath = `M 0 80 Q 250 10 490 80`
+const initialPath = `M 10 100 Q 750 100 1400 100`
+
+const finalPath = `M 10 100 Q 750 100 1400 100`
+
+const string = document.querySelector("#string")
+
+string.addEventListener("mouseenter", function(){
+    console.log("mouse was moved")
+})
+
+
+string.addEventListener("mousemove", function(mouseData){
+    path = `M 10 100 Q ${mouseData.x} ${mouseData.y} 1400 100`
+    
+    gsap.to("svg path", {
+        attr: {d: path},
+        duration: 0.2,
+        ease: "power3.out"
+    })
+})
+
+string.addEventListener("mouseleave", function(){
+    gsap.to("svg path", {
+        attr: {d: finalPath},
+        duration: 1.5,
+        ease: "elastic.out(1, 0.2)"
+    })
+})
